c24074fb4563a6933d2efaf13b2f88e604770953
MBeattie02 <91721899+MBeattie02@users.noreply.github.com>
2022-10-13 13:03:14 +0100

    hosting

--- src/main/java/com/coderscampus/AssignmentSubmissionApp/config/SecurityConfig.java
+++ src/main/java/com/coderscampus/AssignmentSubmissionApp/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http = http.csrf().disable().cors().disable();
+        http = http.csrf().disable().cors().and();
         
         http = http.sessionManagement()
                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
